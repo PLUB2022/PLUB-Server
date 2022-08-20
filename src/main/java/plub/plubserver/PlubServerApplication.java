042281a65d9d97a50f52f05835881ea9888b5575
Seong Heung Bae <mopil1102@gmail.com>
2022-08-20 14:18:00 +0900

    init

---
+++ src/main/java/plub/plubserver/PlubServerApplication.java
@@ -0,0 +1,13 @@
+package plub.plubserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlubServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlubServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/plub/plubserver/PlubServerApplicationTests.java
@@ -0,0 +1,13 @@
+package plub.plubserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlubServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
