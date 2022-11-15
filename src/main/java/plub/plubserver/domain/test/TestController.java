e1e3829134183f6bf1dacadd59443c1b59b6da57
sectionr0 <sectionr0@gmail.com>
2022-11-16 03:22:03 +0900

    [FEAT] 통신 테스트 api 추가
    임시 통신 테스트용 API 입니다.

---
+++ src/main/java/plub/plubserver/domain/test/TestController.java
@@ -0,0 +1,26 @@
+package plub.plubserver.domain.test;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import plub.plubserver.common.dto.ApiResponse;
+
+import static plub.plubserver.common.dto.ApiResponse.success;
+
+@RestController
+public class TestController {
+
+    @PostMapping("/api/test")
+    public ApiResponse<?> testAuthCode(@RequestBody TestDto.AuthCodeRequest authCodeDto) {
+        int statusCode = 200;
+        if (!authCodeDto.isLoginSuccess()) {
+            statusCode = 400;
+        }
+        TestDto.AuthCodeResponse authCodeResponse = new TestDto.AuthCodeResponse(authCodeDto.authCode());
+        return success(
+                statusCode,
+                authCodeResponse,
+                "테스트 용입니다."
+        );
+    }
+}
---
+++ src/main/java/plub/plubserver/domain/test/TestDto.java
@@ -0,0 +1,18 @@
+package plub.plubserver.domain.test;
+
+import io.swagger.annotations.ApiModelProperty;
+
+public class TestDto {
+
+    public record AuthCodeRequest(
+            @ApiModelProperty(value = "통신 테스트용 authCode", example = "AAAAAAAAAAA")
+            String authCode,
+            @ApiModelProperty(value = "통신 테스트용 로그인 성공여부", example = "true/false")
+            boolean isLoginSuccess
+    ) {}
+
+    public record AuthCodeResponse(
+            String authCode
+    ) {}
+
+}
