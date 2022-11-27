b4a4d89fee485e86f78ae099e63037d1711e8b6e
Seong Heung Bae <mopil1102@gmail.com>
2022-11-27 21:54:04 +0900

    [REFACTOR] 모임 생성 엔드포인트 수정

--- src/main/java/plub/plubserver/domain/plubbing/controller/PlubbingController.java
+++ src/main/java/plub/plubserver/domain/plubbing/controller/PlubbingController.java
@@ -2,8 +2,10 @@ package plub.plubserver.domain.plubbing.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import plub.plubserver.common.dto.ApiResponse;
+import plub.plubserver.domain.plubbing.dto.PlubbingDto.CreatePlubbingRequest;
+import plub.plubserver.domain.plubbing.dto.PlubbingDto.PlubbingResponse;
 import plub.plubserver.domain.plubbing.service.PlubbingService;
 
 @RestController
@@ -13,12 +15,12 @@ import plub.plubserver.domain.plubbing.service.PlubbingService;
 public class PlubbingController {
     private final PlubbingService plubbingService;
 
-//    @PostMapping
-//    public ApiResponse<PlubbingResponse> createPlubbing(
-//            @ModelAttribute CreatePlubbingRequest createPlubbingRequest) {
-//        return ApiResponse.success(
-//                plubbingService.createPlubbing(createPlubbingRequest),
-//                "plubbing is successfully created."
-//        );
-//    }
+    @PostMapping
+    public ApiResponse<PlubbingResponse> createPlubbing(
+            @RequestBody CreatePlubbingRequest createPlubbingRequest) {
+        return ApiResponse.success(
+                plubbingService.createPlubbing(createPlubbingRequest),
+                "plubbing is successfully created."
+        );
+    }
 }
