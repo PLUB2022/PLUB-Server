404e79582beabd113b44021399e1634e2a58f4b4
sectionr0 <sectionr0@gmail.com>
2022-12-27 05:40:40 +0900

    [REFACTOR] Valid 오류 메시지 개선
    - ValidationException -> ConstraintViolationException 으로 변경
    - 메시지만 출력 가능하도록 수정

--- src/main/java/plub/plubserver/common/exception/GlobalExceptionHandler.java
+++ src/main/java/plub/plubserver/common/exception/GlobalExceptionHandler.java
@@ -9,11 +9,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException;
 import plub.plubserver.common.dto.ApiResponse;
-import plub.plubserver.common.exception.CommonErrorCode;
 import plub.plubserver.util.s3.AwsS3Exception;
 
-import javax.validation.ValidationException;
+import javax.validation.ConstraintViolationException;
 import java.io.IOException;
+
+import static plub.plubserver.common.exception.CommonErrorCode.*;
 
 @RestControllerAdvice
 @Slf4j
@@ -23,28 +24,28 @@ public class GlobalExceptionHandler {
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public ApiResponse<?> errorHandle(IOException ex) {
         log.warn("예외 발생 및 처리 = {} : {}", ex.getClass().getName(), ex.getMessage());
-        return ApiResponse.error(CommonErrorCode.INVALID_INPUT_VALUE.getStatusCode(), ex.getMessage());
+        return ApiResponse.error(INVALID_INPUT_VALUE.getStatusCode(), ex.getMessage());
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     @ResponseStatus(value = HttpStatus.METHOD_NOT_ALLOWED)
     protected ApiResponse<?> handleHttpRequestMethodNotSupportedException(final HttpRequestMethodNotSupportedException ex) {
         log.warn("예외 발생 및 처리 = {} : {}", ex.getMessage(), ex.getMessage());
-        return ApiResponse.error(CommonErrorCode.METHOD_NOT_ALLOWED.getStatusCode(), ex.getMessage());
+        return ApiResponse.error(METHOD_NOT_ALLOWED.getStatusCode(), ex.getMessage());
     }
 
     @ExceptionHandler(HttpClientErrorException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public ApiResponse<?> httpClientErrorException(HttpClientErrorException ex) {
         log.warn("예외 발생 및 처리 = {} : {}", ex.getClass().getName(), ex.getMessage());
-        return ApiResponse.error(CommonErrorCode.HTTP_CLIENT_ERROR.getStatusCode(), ex.getMessage());
+        return ApiResponse.error(HTTP_CLIENT_ERROR.getStatusCode(), ex.getMessage());
     }
 
-    @ExceptionHandler(ValidationException.class)
+    @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    protected ApiResponse<?> validationException(final ValidationException ex) {
+    protected ApiResponse<?> validationException(final ConstraintViolationException ex) {
         log.warn("예외 발생 및 처리 = {} : {}", ex.getMessage(), ex.getMessage());
-        return ApiResponse.error(CommonErrorCode.INVALID_INPUT_VALUE.getStatusCode(), ex.getMessage());
+        return ApiResponse.error(INVALID_INPUT_VALUE.getStatusCode(), ex.getConstraintViolations().iterator().next().getMessage());
     }
 
     // @Valid 실패시 이 예외가 터져서 잡아줘야 함
@@ -52,7 +53,7 @@ public class GlobalExceptionHandler {
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     protected ApiResponse<?> validationException(final BindException ex) {
         log.warn("예외 발생 및 처리 = {} : {}", ex.getMessage(), ex.getMessage());
-        return ApiResponse.error(CommonErrorCode.INVALID_INPUT_VALUE.getStatusCode(), ex.getMessage());
+        return ApiResponse.error(INVALID_INPUT_VALUE.getStatusCode(), ex.getMessage());
     }
 
     // Aws S3 Error
@@ -60,7 +61,7 @@ public class GlobalExceptionHandler {
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     protected ApiResponse<?> awsS3Error(final AwsS3Exception ex) {
         log.warn("예외 발생 및 처리 = {} : {}", ex.getMessage(), ex.getMessage());
-        return ApiResponse.error(CommonErrorCode.AWS_S3_ERROR.getHttpCode(), ex.getMessage());
+        return ApiResponse.error(AWS_S3_ERROR.getHttpCode(), ex.getMessage());
     }
 
 }
