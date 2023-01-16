8898fdff060d8ab044a13eeb7d22ef766f0f9ee6
sectionr0 <sectionr0@gmail.com>
2023-01-16 16:03:26 +0900

    [FIX] ExceptionHandler 순서 변경
    Order 어노테이션 붙여서 해결.

--- src/main/java/plub/plubserver/domain/account/exception/AuthExceptionHandler.java
+++ src/main/java/plub/plubserver/domain/account/exception/AuthExceptionHandler.java
@@ -1,6 +1,8 @@
 package plub.plubserver.domain.account.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,6 +10,7 @@ import plub.plubserver.common.dto.ApiResponse;
 
 @Slf4j
 @RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class AuthExceptionHandler {
     @ExceptionHandler(AuthException.class)
     public ResponseEntity<?> handle(AuthException ex) {
